65afb908645bc81eba1b9a8a6ca3b6a4f42cce02
Miguel L. Rodrigues <miguellukas52@gmail.com>
2022-01-20 14:35:34 -0300

    downloader

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import os
 
 
@@ -10,6 +10,18 @@ def download(url, path):
         .desc().first().download(path)
 
 
+def download_many(urls, path):
+    for url in urls:
+        download(url, path)
+
+
+def download_playlist(url, path):
+    playlist = Playlist(url)
+
+    for video in playlist.video_urls:
+        download(video, path)
+
+
 if __name__ == '__main__':
     _url = input('Enter the url: ')
 
@@ -18,4 +30,9 @@ if __name__ == '__main__':
     if not os.path.exists(download_path):
         os.mkdir(download_path)
 
-    download(_url, download_path)
+    if _url.lower().__contains__('list'):
+        download_playlist(_url, download_path)
+    else:
+        _urls = _url.split(';')
+
+        download_many(_urls, download_path)
